b316938fa0e63d609613b6c294e07b7fe4945ee0
¨antonio¨ <aramireza15@miumg.edu.gt>
2024-10-16 10:22:15 -0600

    creacion de base de datos en vercel postgresSQL y creacion de login

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,6 +1,11 @@
-function Homepage () {
+function HomePage() {
   return (
-    <div>Homepage</div>
+    <section className="h-[calc(100vh-7rem)] flex justify-center items-center">
+      <div>
+        <h1 className="text-white text-5xl">Home Page</h1>
+      </div>
+    </section>
   )
 }
-export default Homepage
+
+export default HomePage
